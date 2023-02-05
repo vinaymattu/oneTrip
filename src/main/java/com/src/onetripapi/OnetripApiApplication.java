9e992b963e90fd6749e9faf0ad26ab122455d9c4
vinaymattu <vmattu9@gmail.com>
2023-02-05 06:34:20 -0500

    first code

---
+++ src/main/java/com/src/onetripapi/OnetripApiApplication.java
@@ -0,0 +1,13 @@
+package com.src.onetripapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnetripApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnetripApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/src/onetripapi/OnetripApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.src.onetripapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetripApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
